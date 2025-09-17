7bfcf247ec82bfd63bba9f53a334bfcf9fd7397b
Daniel_OV <daos209@gmail.com>
2025-09-17 19:36:55 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rescate-ya',
+  webDir: 'www'
+};
+
+export default config;
